af4dc469a741c69918f10e7ffaa9a39aad25058f
Ahammed  Shorif <79597517+ahammedshorif@users.noreply.github.com>
2024-05-11 00:25:11 +0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,28 +1,28 @@
-CREATE DATABASE social_media_management;
-USE social_media_management;
+CREATE DATABASE social_media_management_system;
+USE social_media_management_system;
 
--- 1.Users table
+-- 1. Users table.
 
 CREATE TABLE users(
     user_id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    user_name varchar(255) UNIQUE NOT NULL,
-    profile_photo varchar(255) DEFAULT 'https://shorturl.at/gNSWY',
-    user_bio varchar(255),
-    create_time TIMESTAMP ,
-    user_gmail varchar(30) NOT NULL,
+    username varchar(255) UNIQUE NOT NULL,
+    profile_photo_url varchar(255),
+    bio varchar(255),
+    created_at TIMESTAMP,
+    email varchar(30) NOT NULL,
 );
 
--- 2.phosos table
+-- 2. photos table.
 
 CREATE TABLE photos(
     photo_id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
     photo_url varchar(255) NOT NULL,
     post_id INT NOT NULL,
-    create_at TIMESTAMP ,
+    created_at TIMESTAMP,
     size float CHECK (size>4)
 );
 
---3.videos table
+--3.videos table.
 
 CREATE TABLE videos(
     video_id INT PRIMARY KEY IDENTITY(1,1),
@@ -32,7 +32,7 @@ CREATE TABLE videos(
     size FLOAT CHECK (size<10)
 );
 
---4.post table
+--4.post table.
 
 CREATE TABLE post(
     post_id INT  PRIMARY KEY IDENTITY(1,1),
@@ -47,7 +47,7 @@ CREATE TABLE post(
     FOREIGN KEY(video_id) REFERENCES videos(video_id)
 );
 
---5.comments table
+--5.comments table.
 
 CREATE TABLE comments (
     comment_id INT PRIMARY KEY IDENTITY(1,1) ,
@@ -59,7 +59,7 @@ CREATE TABLE comments (
     FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
 
---6. post likes table
+--6. post likes table.
 
 CREATE TABLE post_likes (
     user_id INT NOT NULL,
@@ -70,7 +70,7 @@ CREATE TABLE post_likes (
     PRIMARY KEY(user_id, post_id)
 );
 
---7. comment likes table
+--7. comment likes table.
 
 CREATE TABLE comment_likes (
     user_id INT NOT NULL,
@@ -81,7 +81,7 @@ CREATE TABLE comment_likes (
     PRIMARY KEY(user_id, comment_id)
 );
 
---8. follower table
+--8. follower table.
 
 CREATE TABLE follows (
     follower_id INT NOT NULL,
@@ -92,7 +92,7 @@ CREATE TABLE follows (
     PRIMARY KEY(follower_id, followee_id)
 );
 
---9.hashtags table
+--9.hashtags table.
 
 CREATE TABLE hashtags (
   hashtag_id INT PRIMARY KEY IDENTITY(1, 1),
@@ -100,7 +100,7 @@ CREATE TABLE hashtags (
   created_at TIMESTAMP
 );
 
---10.hashtags follow table
+--10.hashtags follow table.
 
 CREATE TABLE hashtag_follow (
 	user_id INT NOT NULL,
@@ -111,7 +111,7 @@ CREATE TABLE hashtag_follow (
     PRIMARY KEY(user_id, hashtag_id)
 );
 
---11.bookmarks table
+--11.bookmarks table.
 
 CREATE TABLE bookmarks (
   post_id INT NOT NULL,
@@ -122,7 +122,7 @@ CREATE TABLE bookmarks (
   PRIMARY KEY(user_id, post_id)
 );
 
--- 12.login table
+-- 12. login table.
 
 CREATE TABLE login (
   login_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
@@ -131,4 +131,3 @@ CREATE TABLE login (
   login_time TIMESTAMP,
   FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
-
